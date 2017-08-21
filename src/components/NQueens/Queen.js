9a51ff98774d56dc4657c139a5b1e23b57c2d609
drstonebraker <drew.stonebraker@gmail.com>
2017-08-20 21:34:27 -0700

    JSX styles demo

--- src/components/NQueens/Queen.js
+++ src/components/NQueens/Queen.js
@@ -6,11 +6,16 @@ export default class Queen extends Component {
 
   render() {
     const { col, currentCol, currentRow } = this.props
+    // const spaceWidth = document.getElementById('n-queens_board').offsetWidth / 8
+    let styles = {
+      'transform': 'translate(100px, 100px)'
+    }
 
     return (
       <div
-        className={`queen ${col <= currentCol && 'u-hidden'}`}
+        className={`queen ${col <= currentCol ? '' : 'u-hidden'}`}
         id={`n-queens_queen--${col}`}
+        style={styles}
       >
         <div
           className="queen__img u-center-both">
